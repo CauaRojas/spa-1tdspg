a764a7ffaf665360faab70a6cd3c5354f2678014
cauarojas <cauarojas@hotmail.com>
2023-09-13 09:34:10 -0300

    added delete to project

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,15 +1,27 @@
-import { useParams } from 'react-router-dom'
-import { getProdutoById } from '../components/ListaProdutos'
+import { useNavigate, useParams } from 'react-router-dom'
+import { getProdutoById, listaProdutos } from '../components/ListaProdutos'
 
 export default function ExcluirProdutos() {
     document.title = 'Excluir Produtos'
     const { id } = useParams()
+    const navigate = useNavigate()
     const produto = getProdutoById(id)
+    const handleDelete = () => {
+        listaProdutos.splice(listaProdutos.indexOf(produto), 1)
+        navigate('/produtos')
+    }
+    const handleCancel = () => {
+        navigate('/produtos')
+    }
     return (
         <>
             <main>
-                <h1>EXCLUIR-PRODUTOS</h1>
-                <p>Produto selecionado - {produto.nome}</p>
+                <h1>EXCLUIR PRODUTOS</h1>
+                <p>{`Deseja realmente excluir o produto: ${produto.nome}`}</p>
+                <div>
+                    <button onClick={handleDelete}>Sim</button>
+                    <button onClick={handleCancel}>Não</button>
+                </div>
             </main>
         </>
     )
